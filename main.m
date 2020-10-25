4f5aaeb7cffb5daf12e3dcf51d02e4c6aaa0d5ff
pi314ever <pilotflyer824@gmail.com>
2020-10-24 17:35:14 -0700

    RK4 and ABM4
    Changed plotting for RK4/ABM4 comparison

--- main.m
+++ main.m
@@ -10,6 +10,7 @@ P.ampl = 0.1; % Amplitude [m]
 P.freq = 100*2*pi(); % Frequency [rad/s]
 th0 = (20)*pi()/180; % Initial angles [rad]
 thd0 = 0; % Initial angular rate [rad/s]
+h = 0.01; % Time step
 tspan = [0 20]; % Time span [s]
 
 % % Double pendulum
@@ -24,20 +25,26 @@ tspan = [0 20]; % Time span [s]
 
 y0 = [th0 thd0];
 %% Simulate
-sol = ode45(@(t,x)Inverted_Single(t,x,P),tspan,y0);
+solRK = RK4(@(t,x)Inverted_Single(t,x,P),tspan,y0,h);
+solABM = ABM4(@(t,x)Inverted_Single(t,x,P),tspan,y0,h);
 
-time = linspace(tspan(1),sol.x(end),1000);
-state = deval(sol,time);
+timeRK = solRK.x;
+stateRK = solRK.y;
+timeABM = solABM.x;
+stateABM = solABM.y;
+% time = linspace(tspan(1),solRK.x(end),1000);
+% state = deval(solRK,time);
 %% Plots
 figure % Time plot
-plot(time,state)
+plot(timeRK,stateRK,timeABM,stateABM,'--','Linewidth',2)
 title('Time plot of states')
 xlabel('Time [s]')
 ylabel('Angle [rad]/Angular rate [rad/s]')
-legend('\theta','\theta''')
+legend('\theta_{RK}','\theta''_{RK}','\theta_{ABM}','\theta''_{ABM}')
 
 figure % Phase portrait
-plot(state(1,:),state(2,:))
+plot(stateRK(1,:),stateRK(2,:),stateABM(1,:),stateABM(2,:),'--','Linewidth',2)
 title('Phase portrait')
 xlabel('Angle [rad]')
 ylabel('Angular rate [rad/s]')
+legend('RK4','ABM4')
