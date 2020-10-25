43f965d0be49fe7e8634d8af485fa0f02efedb90
pi314ever <pilotflyer824@gmail.com>
2020-10-25 15:55:11 -0700

    Updated Plotting
    Improved clarity in plots for presentation.

--- RKABM_Single.m
+++ RKABM_Single.m
@@ -10,7 +10,7 @@ P.l = 1;
 P.m = 1;
 P.ampl = ampl;
 P.freq = freq;
-tspan = [0 20];
+tspan = [0 8];
 solRK = RK4(@(t,x)Inverted_Single(t,x,P),tspan,y0,h);
 solABM = ABM4(@(t,x)Inverted_Single(t,x,P),tspan,y0,h);
 
@@ -21,16 +21,25 @@ stateABM = solABM.y;
 % time = linspace(tspan(1),solRK.x(end),1000);
 % state = deval(solRK,time);
 %% Plots
-figure % Time plot
-plot(timeRK,stateRK,timeABM,stateABM,'--','Linewidth',2)
-title('Time plot of states')
+figure % Theta Time plot
+plot(timeRK,stateRK(1,:),timeABM,stateABM(1,:),'--','Linewidth',2)
+title('Time plot of \theta')
 xlabel('Time [s]')
-ylabel('Angle [rad]/Angular rate [rad/s]')
-legend('\theta_{RK}','\theta''_{RK}','\theta_{ABM}','\theta''_{ABM}')
-Label = sprintf('Ampl = %.2f [m]\nFreq = %.2f [Hz]\ntheta_0 = %.2f [deg]\ntheta_0'' = %.2f [deg/s]\nh = %f [s]',...
-    ampl,freq/(2*pi),y0(1)*180/pi(),y0(2)*180/pi(),h);
+ylabel('Angle [rad]')
+legend('\theta_{RK}','\theta_{ABM}')
+Label = sprintf('Freq = %.2f [Hz]\ntheta_0 = %.2f [deg]\nh = %f [s]',...
+    freq/(2*pi),y0(1)*180/pi(),h);
 annotation('textbox',[0.01 0.92 0 0],'string',Label,'FitBoxToText','on','Fontsize',14)
 set(gca,'fontsize',18)
+
+% figure % Thetad Time plot
+% plot(timeRK,stateRK(2,:),timeABM,stateABM(2,:),'--','Linewidth',2)
+% title('Time plot of \theta''')
+% xlabel('Time [s]')
+% ylabel('Angular rate [rad/s]')
+% legend('\theta''_{RK}','\theta''_{ABM}')
+% annotation('textbox',[0.01 0.92 0 0],'string',Label,'FitBoxToText','on','Fontsize',14)
+% set(gca,'fontsize',18)
 
 figure % Phase portrait
 plot(stateRK(1,:),stateRK(2,:),'b',stateABM(1,:),stateABM(2,:),'r--','Linewidth',2)
@@ -38,8 +47,6 @@ title('Phase portrait')
 xlabel('Angle [rad]')
 ylabel('Angular rate [rad/s]')
 legend('RK4','ABM4')
-Label = sprintf('Ampl = %.2f [m]\nFreq = %.2f [Hz]\ntheta_0 = %.2f [deg]\ntheta_0'' = %.2f [deg/s]\nh = %f [s]',...
-    ampl,freq/(2*pi),y0(1)*180/pi(),y0(2)*180/pi(),h);
 annotation('textbox',[0.01 0.92 0 0],'string',Label,'FitBoxToText','on','Fontsize',14)
 set(gca,'fontsize',18)
 end
--- main.m
+++ main.m
@@ -7,20 +7,20 @@ ampl_default = 0.1; % Amplitude x[m]
 freq_default = 100*2*pi(); % Frequency [rad/s]
 th0_default = (20)*pi()/180; % Initial angles [rad]
 thd0_default = 0; % Initial angular rate [rad/s]
-h_default = 0.01; % Time step
+h_default = 0.001; % Time step
 
 y0_default = [th0_default thd0_default];
 
 RKABM_Single(ampl_default,freq_default,y0_default,h_default);
 %% Changing Amplitude
-ampl = [0 0.3];
-for ii = 1:2
-    RKABM_Single(ampl(ii),freq_default,y0_default,h_default);
-end
+% ampl = [0 0.3];
+% for ii = 1:2
+%     RKABM_Single(ampl(ii),freq_default,y0_default,h_default);
+% end
 
 %% Changing Frequency
-freq = [10 1000]*2*pi();
-for ii = 1:2
+freq = [1 10 1000]*2*pi();
+for ii = 1:length(freq)
     RKABM_Single(ampl_default,freq(ii),y0_default,h_default);
 end
 
@@ -33,15 +33,15 @@ for ii = 1:2
 end
 
 %% Changing thetad_0
-thd0 = [10 30]*pi()/180;
-y0 = y0_default;
-for ii = 1:2
-    y0(2) = thd0(ii);
-    RKABM_Single(ampl_default,freq_default,y0,h_default);
-end
+% thd0 = [10 30]*pi()/180;
+% y0 = y0_default;
+% for ii = 1:2
+%     y0(2) = thd0(ii);
+%     RKABM_Single(ampl_default,freq_default,y0,h_default);
+% end
 
 %% Changing time step
-h = [0.1 0.001];
+h = [0.01 0.0001];
 for ii = 1:2
     RKABM_Single(ampl_default,freq_default,y0_default,h(ii));
 end
